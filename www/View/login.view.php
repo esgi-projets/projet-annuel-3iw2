03c3b6699ca8e8b9f74dc51b1e3e67328f5fb038
LouisAntoine <louisantoine.pro@gmail.com>
2022-02-18 18:28:38 +0100

    SCCS first components and useful variables

--- www/View/login.view.php
+++ www/View/login.view.php
@@ -1,1 +1,31 @@
 <h1>Se connecter</h1>
+
+<button class="button">Hello</button>
+
+<button class="button button--success">Test</button>
+
+<button class="button button--primary">Bouton</button>
+
+<button class="button button--error">Bouton</button>
+
+<button class="button button--secondary">Bouton</button>
+
+<button class="button button--secondary--success">Bouton</button>
+
+<button class="button button--tertiary">Bouton</button>
+
+<button class="button button--tertiary--error">Bouton</button>
+
+<button class="button button--disabled">Bouton</button>
+
+<button class="button button--link">Lien</button>
+
+<button class="button button--link--warning">Lien</button>
+
+<input type="text" class="input" placeholder="Textfield text">
+
+<input type="text" class="input--error" placeholder="Textfield text">
+
+<input type="text" class="input--valid" placeholder="Textfield text">
+
+<input type="text" class="input--disabled" placeholder="Textfield text">
