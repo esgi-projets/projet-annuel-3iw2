5d746d8ac32570f4605d020ae4c97607f642c9bb
Alexandre BETTAN <contact@alexandre.business>
2022-02-23 03:22:54 +0100

    fix scss variables and html for responsive

--- www/View/login.view.php
+++ www/View/login.view.php
@@ -1,26 +1,37 @@
 <div class="row">
   <div class="col-6-xl">
+    <button class="button button--link button--icon"> <svg xmlns="http://www.w3.org/2000/svg" style="width: 24px; height:24px;" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>
+      </svg> Retour
+    </button>
+    <div class="ml-5 container">
+      <img class="logo mx-5" src="./assets/images/BMW_logo_(gray).svg.png" alt="logo">
+      <h1>Se connecter</h1>
+      <h3 class="mb-8">Pour accéder à cette page il est nécessaire de s’authentifier</h3>
 
-    <button class="button button--link">
-      < Retour</button>
-        <div class="login">
-          <img class="logo" src="./assets/images/BMW_logo_(gray).svg.png" alt="logo">
-          <h1>Se connecter</h1>
-          <h3>Pour accéder à cette page il est nécessaire de s’authentifier</h3>
-
-          <label>Email</label>
-          <input type="text" class="input" placeholder="example@test.fr">
-
-          <label>Mot de passe</label>
-          <input type="password" class="input">
-
-          <button class="button button--primary">Se connecter</button>
-          <button class="button button--secondary">Créer un compte</button>
-          <button class="button button--link">Mot de passe oublié ?</button>
+      <div class="pl-5">
+        <div class="column pr-3 pl-3 pb-5">
+          <label class="mb-2">Email</label>
+          <input type="text" class="input w-90" placeholder="example@test.fr">
         </div>
+
+
+        <div class="column pr-3 pl-3">
+          <label class="mb-2">Mot de passe</label>
+          <input type="password" class="input w-90">
+        </div>
+
+        <div class="column mt-8 pr-3 pl-3">
+          <button class="button button--primary w-90">Se connecter</button>
+          <button class="button button--secondary w-90">Créer un compte</button>
+          <button class="button button--link w-90">Mot de passe oublié ?</button>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <div class="col-6-xl">
+  <div class="col-6-xl h-50">
     <img src="./assets/images/dmitry-timofeew-UU18rjWiQmo-unsplash.jpg" alt="car">
   </div>
 
