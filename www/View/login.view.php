b030c20a33c06ad6ac5272d6a697483857e7c384
Alexandre BETTAN <contact@alexandre.business>
2022-02-26 02:50:50 +0100

    responsive with hidden image and flexbox changes

--- www/View/login.view.php
+++ www/View/login.view.php
@@ -1,38 +1,39 @@
 <div class="row">
-  <div class="col-6-xl">
+  <div class="col-6-xl col-12-xs">
     <button class="button button--link button--icon"> <svg xmlns="http://www.w3.org/2000/svg" style="width: 24px; height:24px;" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
       </svg>
       </svg> Retour
     </button>
-    <div class="container-center pl-20 pt-10 pr-10">
-      <img class="logo mx-6" src="./assets/images/BMW_logo_(gray).svg.png" alt="logo">
-      <h1>Se connecter</h1>
-      <h3 class="mb-8">Pour accéder à cette page il est nécessaire de s’authentifier</h3>
+    <div class="container-center">
+      <div class="w-80 mt-4">
+        <img class="logo mx-6" src="./assets/images/BMW_logo_(gray).svg.png" alt="logo">
+        <h1>Se connecter</h1>
+        <h3 class="mb-8">Pour accéder à cette page il est nécessaire de s’authentifier</h3>
 
-      <div class="pl-5">
-        <div class="column pr-3 pl-3 pb-5">
-          <label class="mb-2">Email</label>
-          <input type="text" class="input w-90" placeholder="example@test.fr">
-        </div>
+        <div class="pl-5">
+          <div class="column pr-3 pl-3 pb-5">
+            <label class="mb-2">Email</label>
+            <input type="text" class="input w-100" placeholder="mail@exemple.com">
+          </div>
 
 
-        <div class="column pr-3 pl-3">
-          <label class="mb-2">Mot de passe</label>
-          <input type="password" class="input w-90">
-        </div>
+          <div class="column pr-3 pl-3">
+            <label class="mb-2">Mot de passe</label>
+            <input type="password" class="input w-100">
+          </div>
 
-        <div class="column mt-8 pr-3 pl-3">
-          <button class="button button--primary w-90">Se connecter</button>
-          <button class="button button--secondary w-90">Créer un compte</button>
-          <button class="button button--link w-90">Mot de passe oublié ?</button>
+          <div class="column mt-8 pr-3 pl-3">
+            <button class="button button--primary">Se connecter</button>
+            <button class="button button--secondary">Créer un compte</button>
+            <button class="button button--link">Mot de passe oublié ?</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="col-6-xl flexbox-right">
-    <img class="height-screen" src="./assets/images/dmitry-timofeew-UU18rjWiQmo-unsplash.jpg" alt="car">
+  <div class="col-6-xl hidden-xl">
+    <img src="./assets/images/dmitry-timofeew-UU18rjWiQmo-unsplash.jpg" alt="car">
   </div>
-
 </div>
